445f48aa3ee9864b8b67e1ec3b9d865c36103d02
Cyri0 <nemestamas94@gmail.com>
2026-02-02 16:27:38 +0100

    Add environment configuration and update settings for dotenv integration

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2(-$8*ag!yqk7034&+*u%lkm&711$9b-vaby0pmx56k3*w*pd^'
+if DEBUG:
+    SECRET_KEY = 'django-insecure-2(-$8*ag!yqk7034&+*u%lkm&711$9b-vaby0pmx56k3*w*pd^'
+    ALLOWED_HOSTS = []
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
